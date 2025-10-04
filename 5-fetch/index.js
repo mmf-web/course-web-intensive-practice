57f51d973b06293194bef40bc85f878cd09e27f3
gektus <ar.maklakov@gmail.com>
2025-10-04 11:44:50 +0300

    add 5

---
+++ 5-fetch/index.js
@@ -0,0 +1,93 @@
+// node js & json server
+
+// Fetch start page, fetch HTML
+// fetch('http://localhost:3000')
+//   .then((response) => response.text())
+//   .then((body) => {
+//     document.body.innerHTML = body
+//   })
+
+class Api {
+  async getAllCats() {
+    const r = await fetch('http://localhost:3000/cats')
+    const body = await r.json()
+    return body
+  }
+
+  // ?paramName=pvalue&paramName=paramV
+  // http://localhost:3000/cats?name=toxi%26
+  async getAllCatsByBloodType(types) {
+    const url = new URL('http://localhost:3000/cats')
+    // url.searchParams.set('blood_type', type)
+    types.forEach((type) => {
+      url.searchParams.append('blood_type', type)
+    })
+    // const query = new URLSearchParams({ blood_type: type })
+    const r = await fetch(url)
+    const body = await r.json()
+    return body
+  }
+
+  async createCat(cat) {
+    const r = await fetch('http://localhost:3000/cats', {
+      method: 'POST',
+      body: JSON.stringify(cat),
+    })
+    const body = await r.json()
+    return body
+  }
+
+  async updateCat(id, updates) {
+    const r = await fetch(`http://localhost:3000/cats/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify(updates),
+    })
+    const body = await r.json()
+    return body
+  }
+
+  async deleteCat(id) {
+    const r = await fetch(`http://localhost:3000/cats/${id}`, { method: 'DELETE' })
+    const body = await r.json()
+    return body
+  }
+}
+
+const api = new Api()
+
+async function main() {
+  // await api.updateCat('18ad', { name: 'tesla3' }).then(console.log)
+
+  // await api.deleteCat('d68c').then(console.log)
+
+  await api.getAllCatsByBloodType([1, 2]).then((cats) => {
+    let table = `
+  <table>
+    <thead>
+        <tr>
+          <td>id</td>
+          <td>name</td>
+          <td>is black</td>
+          <td>blood type</td>
+        </tr>
+    <thead>
+    <tbody>
+  `
+    cats.forEach((cat) => {
+      table += `
+      <tr>
+        <td>${cat.id}</td>
+        <td>${cat.name}</td>
+        <td>${cat.is_black}</td>
+        <td>${cat.blood_type}</td>
+      </tr>
+    `
+    })
+    table += `</tbody></table>`
+    document.body.innerHTML = table
+  })
+
+  // api.createCat({ name: 'tesla' }).then(console.log)
+}
+
+main()
